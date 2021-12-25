f6ea199c9ad857654b7d7359b1b95e11f93077f5
zzl <zzl@atguigu.com>
2021-12-25 21:49:49 +0800

    master commit1

--- src/main/java/git_Test.java
+++ src/main/java/git_Test.java
@@ -1,11 +1,7 @@
-/**
- * @Author zzl
- * @Date 2021/12/25
- * @Describe 在IDEA中使用Git
- */
 public class git_Test {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println("ggg");
+        System.out.println("master");
     }
 }
