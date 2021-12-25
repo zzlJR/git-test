62c5c4a9187da1ced06dca23a45f893b5f454f40
zzl <zzl@atguigu.com>
2021-12-25 21:50:53 +0800

    hot-fix commit1

--- src/main/java/git_Test.java
+++ src/main/java/git_Test.java
@@ -7,5 +7,6 @@ public class git_Test {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println("ggg");
+        System.out.println("hot-fix");
     }
 }
